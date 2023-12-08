561dceebc8feba78da15b071b500c479c4f3a149
xkhang <xkhang.nguyen2bme@gmail.com>
2023-12-08 11:21:51 +0700

    Make responsive header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AssetModule } from './modules/asset-module/asset/asset.module';
 import { StageOfLifeModule } from './modules/stage-of-life-module/stage-of-life/stage-of-life.module';
+import { ShareModule } from './modules/share-module/share.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AssetModule, StageOfLifeModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    AssetModule,
+    StageOfLifeModule,
+    ShareModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/modules/share-module/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { SCREEN_SIZE } from '../../../services/size-detector/screen-size.enum';
+import { ResizeService } from '../../../services/size-detector/resize.service';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  size!: SCREEN_SIZE;
+
+  constructor(private resizeSvc: ResizeService) {
+    this.resizeSvc.onResize$.pipe(delay(0)).subscribe((x) => {
+      this.size = x;
+      console.log(x);
+    });
+  }
+}

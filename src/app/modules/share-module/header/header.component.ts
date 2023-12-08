44e98f09a30c6353afdf012209b4900b7675e78d
Khang <xkhang.nguyen2bme@gmail.com>
2023-12-09 02:15:44 +0700

    Add responsive header

--- src/app/modules/share-module/header/header.component.ts
+++ src/app/modules/share-module/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { SCREEN_SIZE } from '../../../services/size-detector/screen-size.enum';
-import { ResizeService } from '../../../services/size-detector/resize.service';
-import { delay } from 'rxjs/operators';
+import { ResponsiveService } from '../../../services/responsive/responsive.service';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +7,5 @@ import { delay } from 'rxjs/operators';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  size!: SCREEN_SIZE;
-
-  constructor(private resizeSvc: ResizeService) {
-    this.resizeSvc.onResize$.pipe(delay(0)).subscribe((x) => {
-      this.size = x;
-    });
-  }
+  constructor(public responsiveService: ResponsiveService) {}
 }
--- src/app/modules/share-module/share.module.ts
+++ src/app/modules/share-module/share.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { ResizeService } from '../../services/size-detector/resize.service';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [CommonModule],
   exports: [HeaderComponent],
-  providers: [ResizeService],
 })
 export class ShareModule {}

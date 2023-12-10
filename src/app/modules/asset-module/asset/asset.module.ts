fb094cf8cff780a91608aed85db6bfce2185bc85
DuyAKA <92700560+DuyAKA@users.noreply.github.com>
2023-12-10 10:58:12 +0700

    navtabs still loading

--- src/app/modules/asset-module/asset/asset.module.ts
+++ src/app/modules/asset-module/asset/asset.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AssetComponent } from './asset.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [AssetComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTabsModule],
 })
 export class AssetModule {}
